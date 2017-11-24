f55017a5aaf581afc054087a115c4b526994eb90
Mateusz Ligeza <mateusz.kamil.ligeza@gmail.com>
2017-11-25 00:15:22 +0100

    Remove Example

--- SeleniumForRNApdbee/Example.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-
-driver = webdriver.Chrome("C:/Users/zaloguj/IdeaProjects/RNApdbee/driver/chromedriver.exe")
-
-driver.get('http://rnapdbee.cs.put.poznan.pl')
-
-for i in driver.find_elements_by_name("basePairAnalyzer"):
-    i.click()
